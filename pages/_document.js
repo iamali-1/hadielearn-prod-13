bd5ad225fc52eccd46fc7844a7afd39b687c39e5
HadiScript <2019cs519@gmail.com>
2024-04-22 15:07:49 +0500

    no script2

--- pages/_document.js
+++ pages/_document.js
@@ -73,11 +73,11 @@ export default function Document() {
           integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
           crossOrigin="anonymous"
         ></script> */}
-        <script
+        {/* <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
           integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
           crossOrigin="anonymous"
-        ></script>
+        ></script> */}
 
         {/* ads */}
         <script
