c5af236cbc4bc4664028e2c255ef5b2ec9a8c427
HadiScript <2019cs519@gmail.com>
2024-07-26 10:49:59 +0500

    push for new

--- pages/panel/admin/index.jsx
+++ pages/panel/admin/index.jsx
@@ -1,9 +1,17 @@
 import AdminLayout from "@/components/panel/admin/AdminLayout";
 import React from "react";
+import { useAuth } from "../../../context/authContext";
 
 const Admin = () => {
+
+  const [auth] = useAuth()
+
+
   return <AdminLayout>
-    <p>You are currently login with viewer account, so you will have limited options only..</p>
+    {
+      auth?.user?.role === "admin" ? <p>Welcome Mr. Admin</p>
+        : <p>You are currently login with viewer account, so you will have limited options only..</p>
+    }
   </AdminLayout>;
 };
 
