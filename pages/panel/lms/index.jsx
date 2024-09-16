95c9a471642b4e23053e099fde08c0531af9194a
hadi script <hadistructures@gmail.com>
2024-09-16 16:04:19 +0500

    LMS error fixed

--- pages/panel/lms/index.jsx
+++ pages/panel/lms/index.jsx
@@ -1,58 +1,59 @@
 import LMSLayout from "@/components/panel/lms/LMSLayout";
 import Stats2 from "@/components/ui/common/Stats2";
+import { API } from "@/config/APIs";
+import axios from "axios";
 
-import { Card, Grid } from "antd";
-import { useState } from "react";
+// import { Card, Grid } from "antd";
+// import { useState } from "react";
 
-import CountUp from "react-countup";
-import VisibilitySensor from "react-visibility-sensor";
+// import CountUp from "react-countup";
+// import VisibilitySensor from "react-visibility-sensor";
 
+// const CounterBox = ({ counter, title, color, image }) => {
+//   const [viewCountUp, setViewCountUp] = useState(false);
 
+//   const onVisibilityChange = (isVisible) => {
+//     if (isVisible) {
+//       setViewCountUp(true);
+//     }
+//   };
+//   return (
+//     <div className="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12 mb-1 ">
+//       <Card hoverable>
+//         <div className="d-flex align-items-center gap-3" style={{ color: "#135c6c" }}>
 
-const CounterBox = ({ counter, title, color, image }) => {
-  const [viewCountUp, setViewCountUp] = useState(false);
+//           <div className="d-flex flex-column ">
+//             <b style={{ color: "#135c6c", fontSize: "30px" }}>
+//               <VisibilitySensor onChange={onVisibilityChange} offset={{ top: 10 }} delayedCall>
+//                 <>
+//                   <CountUp end={viewCountUp ? counter : 0} duration={8} /> <span>+</span>
+//                 </>
+//               </VisibilitySensor>
+//             </b>
+//             <span>{title}</span>
+//           </div>
+//         </div >
+//       </Card>
+//     </div >
+//   )
+// }
 
-  const onVisibilityChange = (isVisible) => {
-    if (isVisible) {
-      setViewCountUp(true);
-    }
-  };
+const LMS = ({ stats }) => {
   return (
-    <div className="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12 mb-1 ">
-      <Card hoverable>
-        <div className="d-flex align-items-center gap-3" style={{ color: "#135c6c" }}>
-
-          <div className="d-flex flex-column ">
-            <b style={{ color: "#135c6c", fontSize: "30px" }}>
-              <VisibilitySensor onChange={onVisibilityChange} offset={{ top: 10 }} delayedCall>
-                <>
-                  <CountUp end={viewCountUp ? counter : 0} duration={8} /> <span>+</span>
-                </>
-              </VisibilitySensor>
-            </b>
-            <span>{title}</span>
-          </div>
-        </div >
-      </Card>
-    </div >
-  )
-}
-
-const LMS = () => {
-  return <LMSLayout>
-    {/* <div className="row mb-30"> */}
-
-    {/* <CounterBox counter={1500} title="Total Enrollments" image={'live_classes'} />
-      <CounterBox counter={30000} title="Courses" image={'enrolled_Students'} />
-      <CounterBox counter={3000} title="Workshops" image={'students_on_waitlist'} />
-      <CounterBox counter={50000} title="Students" image={'queries_answered'} /> */}
-
-    <Stats2 from="admins" />
-
-    {/* </div> */}
-
-
-  </LMSLayout>;
+    <LMSLayout>
+      <Stats2 from="admins" stats={stats} />
+    </LMSLayout>
+  );
 };
+
+export async function getServerSideProps() {
+  const res = await axios.get(`${API}/stat/all-stats`);
+
+  return {
+    props: {
+      stats: res.data,
+    },
+  };
+}
 
 export default LMS;
